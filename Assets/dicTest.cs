c8fdc2f891b951052db417ef8594bf28f20440d1
unknown <y_okada@vmyo8x64devU.kimoto-electric.co.jp>
2015-09-15 08:27:09 +0900

    show <int,int> dictionary

---
+++ Assets/dicTest.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic; // for Dictionary
+
+public class dicTest : MonoBehaviour {
+
+	void Test_dictionary() {
+		Dictionary <int, int> myDic = new Dictionary<int, int> ();
+
+		myDic.Add (3, 1415);
+		myDic.Add (2, 7182);
+
+		foreach (KeyValuePair<int, int> pair in myDic) {
+			Debug.Log(pair.Key + " : " + pair.Value);
+		}
+	}
+
+	void Start () {
+		Test_dictionary ();
+	}	
+}
